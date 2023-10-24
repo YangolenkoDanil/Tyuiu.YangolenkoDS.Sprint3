06b719f86dc6ee31794c49134548b50b2ddfa9a5
Tyuiu.YangolenkoDS@outlook.com <angolenkodanil@gmail.com>
2023-10-24 10:47:47 +0500

    Task1

--- Tyuiu.YangolenkoDS.Sprint3.Task1.V6/Program.cs
+++ Tyuiu.YangolenkoDS.Sprint3.Task1.V6/Program.cs
@@ -14,7 +14,7 @@ namespace Tyuiu.YangolenkoDS.Sprint3.Task1.V6
         {
             DataService ds = new DataService();
 
-            Console.Title = "Спринт #2 | Выполнил: Янголенко Д. С. | АСОиУб-23-1";
+            Console.Title = "Спринт #3 | Выполнил: Янголенко Д. С. | АСОиУб-23-1";
             Console.WriteLine("*************************************************************************************");
             Console.WriteLine("* Спринт #3                                                                         *");
             Console.WriteLine("* Тема: Оператор цикла while                                                        *");
